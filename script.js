294a27bfd8e03a88fc62739f32632d9015ecf6d0
Delstroo <delstunmccray@gmail.com>
2023-09-20 12:44:21 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,70 +1,81 @@
-document.getElementById("submit").addEventListener("click", function () {
-    var form = document.getElementById("feedbackForm");
-    var loadingIndicator = document.getElementById("loading");
-    var successIndicator = document.getElementById("success");
-    var messageElement = document.getElementById("message");
-    var emailField = form.email;
-    var descriptionField = form.description;
-    var submitButton = form.submit; // Get the submit button
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to toggle the submit button state based on input values
+    function toggleSubmitButtonState() {
+        var emailField = document.getElementById("Email");
+        var descriptionField = document.getElementById("Description");
+        var submitButton = document.getElementById("submit");
 
-    // Check if email and description fields are empty
-    if (emailField.value.trim() === "" || descriptionField.value.trim() === "") {
-        messageElement.innerText = "Please fill out both email and description fields.";
-        return; // Prevent further execution
+        // Check if both email and description fields are filled out
+        submitButton.disabled = emailField.value.trim() === "" || descriptionField.value.trim() === "";
     }
 
-    // Disable the submit button
-    submitButton.disabled = true;
+    // Add an event listener to check for input changes in email and description fields
+    document.getElementById("Email").addEventListener("input", toggleSubmitButtonState);
+    document.getElementById("Description").addEventListener("input", toggleSubmitButtonState);
 
-    // Hide the submit button and show the loading indicator
-    submitButton.style.display = "none";
-    loadingIndicator.style.display = "block";
+    // Add a submit event listener to the form
+    document.getElementById("submit-to-google-sheet").addEventListener("submit", function (e) {
+        e.preventDefault();
 
-    // Simulate a delay to demonstrate the loading indicator (remove this in production)
-    setTimeout(function () {
-        // Simulate form submission (replace with actual form submission logic)
-        // Here, we are just clearing the text fields as a demonstration
-        emailField.value = "";
-        descriptionField.value = "";
+        // Your form submission logic here
+        var form = e.target;
+        var loadingIndicator = document.getElementById("loading");
+        var successIndicator = document.getElementById("success");
+        var messageElement = document.getElementById("message");
+        var emailField = form.elements["Email"];
+        var descriptionField = form.elements["Description"];
+        var submitButton = form.elements["submit"];
 
-        // Hide the loading indicator and show the success indicator
-        loadingIndicator.style.display = "none";
-        successIndicator.style.display = "block";
+        // Check if email and description fields are empty
+        if (emailField.value.trim() === "" || descriptionField.value.trim() === "") {
+            messageElement.innerText = "Please fill out both email and description fields.";
+            return; // Prevent further execution
+        }
 
-        // Display a success message and clear it after a delay
-        messageElement.innerText = "Form submitted successfully!";
+        // Disable the submit button
+        submitButton.disabled = true;
+
+        // Hide the submit button and show the loading indicator
+        submitButton.style.display = "none";
+        loadingIndicator.style.display = "block";
+
+        // Simulate a delay to demonstrate the loading indicator (remove this in production)
         setTimeout(function () {
-            messageElement.innerText = "";
-        }, 3000);
+            // Simulate form submission (replace with actual form submission logic)
+            // Here, we are just clearing the text fields as a demonstration
+            emailField.value = "";
+            descriptionField.value = "";
 
-        // Hide the success indicator after a delay
-        setTimeout(function () {
-            successIndicator.style.display = "none";
+            // Hide the loading indicator and show the success indicator
+            loadingIndicator.style.display = "none";
+            successIndicator.style.display = "block";
 
-            // Re-enable the submit button
-            submitButton.disabled = false;
+            // Display a success message and clear it after a delay
+            messageElement.innerText = "Form submitted successfully!";
+            setTimeout(function () {
+                messageElement.innerText = "";
+            }, 3000);
 
-            // Show the submit button again
-            submitButton.style.display = "block";
-        }, 3000);
+            // Hide the success indicator after a delay
+            setTimeout(function () {
+                successIndicator.style.display = "none";
 
-    }, 2000); // Simulated submission delay of 2 seconds (remove in production)
+                // Re-enable the submit button
+                submitButton.disabled = false;
+
+                // Show the submit button again
+                submitButton.style.display = "block";
+            }, 3000);
+
+        }, 2000); // Simulated submission delay of 2 seconds (remove in production)
+
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbwIzUWyxoxtIucwsQuh06bkcQSIZWTTrpwZHgO9l5bUBvt4mA6AkTeyf__cvCDHyZzy/exec';
+
+        form.addEventListener('submit', e => {
+            e.preventDefault()
+            fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+              .then(response => console.log('Success!', response))
+              .catch(error => console.error('Error!', error.message))
+          })
+    });
 });
-// Function to toggle the submit button state based on input values
-function toggleSubmitButtonState() {
-    var emailField = document.getElementById("email");
-    var descriptionField = document.getElementById("description");
-    var submitButton = document.getElementById("submit");
-
-    // Check if both email and description fields are filled out
-    if (emailField.value.trim() !== "" && descriptionField.value.trim() !== "") {
-        submitButton.disabled = false; // Enable the submit button
-    } else {
-        submitButton.disabled = true; // Disable the submit button
-    }
-}
-
-// Add an event listener to check for input changes in email and description fields
-document.getElementById("email").addEventListener("input", toggleSubmitButtonState);
-document.getElementById("description").addEventListener("input", toggleSubmitButtonState);
-
